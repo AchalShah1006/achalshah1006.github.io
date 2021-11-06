e1629ff934d6d0b84ed740a1bdbd90ba418369e1
AchalShah1006 <shahachal.32@gmail.com>
2021-11-07 01:40:45 +0530

    changed to hashrouter for gh-pages

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 import HomePage from '../HomePage';
 import Resume from '../../components/Resume';
